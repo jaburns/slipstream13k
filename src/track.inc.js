476a7986fd5bb3ac131af0789f0543c185e35fad
Jeremy Burns <j@jaburns.net>
2019-09-11 15:36:16 -0600

    Golfing track.inc.js

--- src/track.inc.js
+++ src/track.inc.js
@@ -3,32 +3,28 @@ let track_nodes;
 let track_parseUploadedCurveHandles = pts => {
     track_nodes = [];
 
-    let bezier = (ax, ay, bx, by, cx, cy, dx, dy, t, u) => [
-        u*u*u*ax + 3*u*u*t*bx + 3*u*t*t*cx + t*t*t*dx,
-        u*u*u*ay + 3*u*u*t*by + 3*u*t*t*cy + t*t*t*dy,
-    ];
+    let bezierMap = (a, b, c, d, t, u) => u*u*u*a + 3*u*u*t*b + 3*u*t*t*c + t*t*t*d;
 
     for (let i = 0; i < pts.length; i += 6) {
         let j = (i + 6) % pts.length;
         for (let t = 0; t < 1; t += 0.05)
-            track_nodes.push(bezier(pts[i],pts[i+1],pts[i+2],pts[i+3],pts[i+4],pts[i+5],pts[j],pts[j+1],t,1-t));
+            track_nodes.push([
+                bezierMap(pts[i], pts[i+2], pts[i+4], pts[j], t, 1-t),
+                bezierMap(pts[i+1], pts[i+3], pts[i+5], pts[j+1], t, 1-t)
+            ]);
     }
 };
 
-let track_getLapPosition = pos3 => {
-    let minDist = 1e9;
-    let minI = -1;
-    for (let i = 0; i < track_nodes.length; ++i) {
-        let dx = pos3[0] - track_nodes[i][0]*G_TERRAIN_WORLDSPACE_SIZE;
-        let dz = pos3[2] - track_nodes[i][1]*G_TERRAIN_WORLDSPACE_SIZE;
-        let d2 = dx*dx+dz*dz;
-        if (d2 < minDist) {
-            minDist = d2;
-            minI = i;
-        }
-    }
-    return minI / track_nodes.length;
-};
+let track_getLapPosition = pos3 =>
+    track_nodes
+        .map((n,dx,dz) => (
+            dx = pos3[0] - n[0]*G_TERRAIN_WORLDSPACE_SIZE,
+            dz = pos3[2] - n[1]*G_TERRAIN_WORLDSPACE_SIZE,
+            [dx*dx+dz*dz, i]
+        ))
+        .sort((a,b) => b[0] - a[0])
+        .pop()[1]
+            / track_nodes.length;
 
 let track_getStartYaw = () => 
     Math.atan2(track_nodes[0][0]-track_nodes[1][0], track_nodes[0][1]-track_nodes[1][1]);
